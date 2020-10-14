c61ff3524144ab25d6c87b03cef95f92c8f5c0df
Matteo Harchi <matteo.harchi@gmail.com>
2020-10-14 20:21:25 +0200

    database

--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -7,11 +7,11 @@
         <div class="d-flex cards">
             @foreach ($data as $key=>$student)
                 <div class="card" style="width: 20rem;">
-                    <img src="{{$student['img']}}" class="card-img-top" alt="...">
+                    <img src="{{$student->img}}" class="card-img-top" alt="...">
                     <div class="card-body">
-                    <h5 class="card-title"> <a href="{{route('Student.show', ['id'=>$key])}}">{{$student['name']}}</a>  ({{$student['anni']}} anni)</h5>
-                    <h6>Assunt{{($student['genere'] == 'f' ? 'a' : 'o')}} da {{$student['azienda']}}</h6>
-                    <p class="card-text">{{$student['descrizione']}}</p>
+                    <h5 class="card-title"> <a href="{{route('Student.show', ['id'=>$student->id])}}">{{$student['name']}}</a>  ({{$student['anni']}} anni)</h5>
+                    <h6>Assunt{{($student->genere == 'f' ? 'a' : 'o')}} da {{$student->azienda}}</h6>
+                    <p class="card-text">{{$student->descrizione}}</p>
                     <div class="card-linkedin-icon"><a href="#"><i class="fab fa-linkedin"></i></a></div>
                     </div>
                 </div>   
